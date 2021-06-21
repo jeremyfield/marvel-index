0395b55d2634b30e76728b56d1e8d7a5b0e99615
Jeremy Field <jeremyfield@protonmail.com>
2021-06-20 20:49:15 -0700

    scroll loading for comics

--- app/routes/characters.js
+++ app/routes/characters.js
@@ -5,6 +5,9 @@ export default class CharactersRoute extends Route {
   @service infinity;
 
   model() {
-    return this.infinity.model('character', { perPageParam: 'limit' });
+    return this.infinity.model('character', {
+      perPageParam: 'limit',
+      perPage: 24,
+    });
   }
 }
--- app/routes/comics.js
+++ app/routes/comics.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class ComicsRoute extends Route {
+  @service infinity;
+
   model() {
-    return this.store.findAll('comic');
+    return this.infinity.model('comic', { perPageParam: 'limit', perPage: 24 });
   }
 }
