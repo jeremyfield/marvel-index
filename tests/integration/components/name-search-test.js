ba26f0a5d5508c562d1ca122e2a2d43bece6cc32
Jeremy Field <jeremyfield@protonmail.com>
2021-06-23 00:20:17 -0700

    fixed name-search-test

--- tests/integration/components/name-search-test.js
+++ tests/integration/components/name-search-test.js
@@ -3,24 +3,15 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | name-search', function(hooks) {
+module('Integration | Component | name-search', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
     await render(hbs`<NameSearch />`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <NameSearch>
-        template block text
-      </NameSearch>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(this.element.textContent.trim(), 'Search by name:');
   });
 });
